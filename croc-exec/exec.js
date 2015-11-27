54e36bb9debd545ec1adeecf32a87089bd0c104d
Sigurd Fosseng <sigurd@fosseng.net>
2015-11-27 16:48:10 +0000

    standard syntax

--- croc-exec/exec.js
+++ croc-exec/exec.js
@@ -1,17 +1,17 @@
-var deps = require('croc-deps');
-var path = require('path');
-var shelljs = require('shelljs');
+var deps = require('croc-deps')
+var path = require('path')
+var shelljs = require('shelljs')
 
-exports.exec = function(command, latient) {
-  var order = deps.order(latient || false);
+exports.exec = function (command, latient) {
+  var order = deps.order(latient || false)
   order
-    .forEach(function(pkgInfo) {
+    .forEach(function (pkgInfo) {
       var cmd = command
         .replace('%PKG_NAME%', pkgInfo[0])
-        .replace('%PKG_VERSION%', pkgInfo[1]);
+        .replace('%PKG_VERSION%', pkgInfo[1])
 
-      shelljs.cd(path.dirname(pkgInfo[3]));
-      console.error(cmd);
-      shelljs.exec(cmd);
-    });
-};
+      shelljs.cd(path.dirname(pkgInfo[3]))
+      console.error(cmd)
+      shelljs.exec(cmd)
+    })
+}
--- croc-link/link.js
+++ croc-link/link.js
@@ -1,25 +1,24 @@
-var deps = require('croc-deps');
-var list = require('croc-list');
-var path = require('path');
-var shelljs = require('shelljs');
+var deps = require('croc-deps')
+var list = require('croc-list')
+var path = require('path')
+var shelljs = require('shelljs')
 
-exports.link = function(options) {
-  var opts = options || {};
-  var pkgs = list.packages();
-  var order = deps.order({ lenient: opts.lenient });
-  
-  order.forEach(function(pkgInfo) {
-    var pkg = pkgInfo[0];
-    var deps = pkgInfo[2];
-    var pkgjson = pkgInfo[3];
+exports.link = function (options) {
+  var opts = options || {}
+  var pkgs = list.packages()
+  var order = deps.order({ lenient: opts.lenient })
 
-    var mapping = deps.forEach(function(dep) {
-      var dName = dep.split('#')[0];
-        var linkPath = path.join(path.dirname(pkgjson), 'node_modules');
-        var link = path.join(linkPath, dName);
-        var targetPath = path.dirname(pkgs[dName].file);
-        shelljs.mkdir('-p', linkPath);
-        shelljs.ln('-sf', targetPath, link);
-    });
-  });
-};
+  order.forEach(function (pkgInfo) {
+    var deps = pkgInfo[2]
+    var pkgjson = pkgInfo[3]
+
+    deps.forEach(function (dep) {
+      var dName = dep.split('#')[0]
+      var linkPath = path.join(path.dirname(pkgjson), 'node_modules')
+      var link = path.join(linkPath, dName)
+      var targetPath = path.dirname(pkgs[dName].file)
+      shelljs.mkdir('-p', linkPath)
+      shelljs.ln('-sf', targetPath, link)
+    })
+  })
+}
