d78bdcf1ae90951c5f6c2ac2122968f3ebe5d28c
Sigurd Fosseng <sigurd.fosseng@sparebank1.no>
2016-01-18 14:28:28 +0100

    link binaries

--- croc-link/link.js
+++ croc-link/link.js
@@ -2,6 +2,31 @@ var deps = require('croc-deps')
 var path = require('path')
 var shelljs = require('shelljs')
 
+function getBinaries (pkg) {
+  var name = pkg.name
+  var bin = pkg.bin
+  var bins = []
+  if (typeof bin === 'string') {
+    bins = [{name, bin}]
+  } else {
+    bins = Object.keys(bin).map(name => {
+      return {name, bin: bin[name]}
+    })
+  }
+  return bins
+}
+
+function getBinFiles (pkgInfo) {
+  var bins = getBinaries(pkgInfo)
+  var basepath = path.dirname(pkgInfo._file)
+  return bins.map(function (bin) {
+    return {
+      file: path.join(basepath, bin.bin),
+      name: bin.name
+    }
+  })
+}
+
 exports.link = function (pkgs) {
   var order = deps.order(pkgs)
 
@@ -14,9 +39,24 @@ exports.link = function (pkgs) {
       var linkPath = path.join(path.dirname(pkgjson), 'node_modules')
       var link = path.join(linkPath, name)
       var targetPath = path.dirname(pkgs.node(name)._file)
-      shelljs.mkdir('-p', linkPath)
+
+      shelljs.mkdir('-p', path.dirname(link)) // scoped packages
       shelljs.ln('-sf', targetPath, link)
       console.error('ln -sf ' + targetPath + ' ' + link)
+
+      if (pkgs.node(name).bin) {
+        var binPackage = pkgs.node(name)
+        var binPath = path.join(linkPath, '.bin')
+        shelljs.mkdir('-p', binPath)
+        getBinFiles(binPackage).forEach(function (bin) {
+          var targetPath = path.join(binPath, bin.name)
+          console.error('chmod u+x ' + bin.file)
+          shelljs.chmod('u+x', bin.file)
+          shelljs.rm(targetPath)
+          console.error('ln -sf ' + bin.file + ' ' + targetPath)
+          shelljs.ln('-sf', bin.file, targetPath)
+        })
+      }
     })
   })
 }
